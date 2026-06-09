f1bdb7c41c7dccd7bb55095ceab8659a9134d5d4
saifali-arswift <saifali@arswift.com>
2026-06-09 17:57:11 +0500

    feat: implement user registration schema and add authentication dependencies

---
+++ models/User.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+// Blueprint (Schema) of user
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required']
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      lowercase: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required']
+    }
+  },
+
+  {
+    timestamps: true
+  }
+)
+
+export const User = mongoose.models.User || mongoose.model('User', userSchema)
