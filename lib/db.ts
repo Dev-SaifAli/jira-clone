a65018f6a2c4562707d14c172c5c653c877346d4
Dev-SaifAli <saifu100@outlook.com>
2026-06-17 18:28:54 +0500

    feat: implement database connection logic with mongoose

---
+++ lib/db.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+// define the type of mongoose variable inside the global object
+declare global {
+  var mongoose: any
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null }
+}
+
+async function dbConnect () {
+  const MONGODB_URI = process.env.MONGODB_URI
+
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is not defined in the .env')
+  }
+
+  if (cached.conn) {
+    return cached.conn
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false
+    }
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then(mongoose => {
+      return mongoose
+    })
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+
+  return cached.conn
+}
+
+export default dbConnect
